b8f526ef7579bf5946ae6917ea9c2143a5af7658
Borisz Bisits <118134911+BoriszBisits@users.noreply.github.com>
2023-03-21 11:13:04 +0100

    showfirst

--- 1/showFirst.js
+++ 1/showFirst.js
@@ -6,6 +6,10 @@
 const getBeers = require('../data')
 
 function main() {
+    let beer=getBeers();
+
+    //console.log(`${name} (${brewery})`)
+    console.log(beer[0].name,beer[0].brewery)
 }
 
 main()
